88a09540545ca678c4db1b01c6e03c3c381dd446
Papon khan <paponk25@gmail.com>
2026-05-05 01:24:29 +0600

    some

--- Index.js
+++ Index.js
@@ -1,17 +1,36 @@
 //Server runing and mounting application in this file
 import express from "express";
-import cors from "cors";
+import corsOrigin from "./src/utils/corsOrigin/corsOrigin.js";
+import globalError from "./src/middleware/globalError/globalError.js";
+import connectDB from "./src/config/database/database.js";
 
 //express app start area
 const app = express();
 //express app start area
 //cors policy include
-cors()
+app.use(corsOrigin);
 //cors policy include
+//Body parser area
+app.use(
+  express.json({
+    limit: "10kb",
+    strict: true,
+    type: ["application/json", "text/plain"],
+  }),
+);
+//Body parser area
+//all routing
 
+//all routing
+//global error handler
+app.use(globalError);
+//global error handler
 //server listen area
 const serverListen = async () => {
   try {
+    //Database connection
+    await connectDB();
+    //Database connection
     //server create
     const port = process.env.PORT;
     app.listen(port, () => {
